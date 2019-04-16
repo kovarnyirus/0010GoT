c4a1fd1b6f6dc94767073825fea6ecd99194771d
kovarnyirus <kovarnyi@gmail.com>
2019-04-16 11:08:04 +0300

    правки

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -1,22 +1,6 @@
 // Для лэзи загрузки
 
 
-if ($("div").is("#s-map-cemetery")) {
-    console.log("1515");
-    $("body").append("<div class=\"map-tutorial\"><a class=\"map-tutorial__close text-primary\">x</a>\n" +
-        "      <button class=\"form-wrap__btn\">понятно</button>\n" +
-        "    </div>");
-    $('body').on('click', '.form-wrap__btn', function(){
-        console.log("122222");
-        $('.map-tutorial').remove()
-    });
-    $('body').on('click', '.map-tutorial__close', function(){
-        console.log("122222");
-        $('.map-tutorial').remove()
-    });
-}
-});
-
 var JSCCommon = {
 
     LazyFunction: function () {
@@ -434,6 +418,20 @@ jQuery(document).ready(function ($) {
     //     // adaptiveHeight: true,
     //     arrows: false
     // });
+
+
+    if ($("div").is("#s-map-cemetery")) {
+        $("body").append("<div class=\"map-tutorial\"><a class=\"map-tutorial__close text-primary\">x</a>\n" +
+            "      <button class=\"form-wrap__btn\">понятно</button>\n" +
+            "    </div>");
+        $('body').on('click', '.form-wrap__btn', function(){
+            $('.map-tutorial').remove()
+        });
+        $('body').on('click', '.map-tutorial__close', function(){
+            $('.map-tutorial').remove()
+        });
+    }
+});
 
 
 
